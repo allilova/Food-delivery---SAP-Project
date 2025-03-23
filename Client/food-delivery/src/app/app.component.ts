e11ab31ba2ffbb700a48e5eadaf4b7c8f293079c
allilova <80039522+allilova@users.noreply.github.com>
2025-03-23 20:42:28 +0200

    nav-bar/footer

--- Client/food-delivery/src/app/app.component.ts
+++ Client/food-delivery/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationBarComponent} from './nav-bar/nav-bar.component';
+import{FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationBarComponent],
+  imports: [RouterOutlet, NavigationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

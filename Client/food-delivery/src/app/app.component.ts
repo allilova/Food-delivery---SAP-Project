bbfde57787847f3fe244ab02a5ac2d159193ecd7
allilova <80039522+allilova@users.noreply.github.com>
2025-03-24 10:08:25 +0200

    login page

--- Client/food-delivery/src/app/app.component.ts
+++ Client/food-delivery/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationBarComponent} from './nav-bar/nav-bar.component';
 import{FooterComponent} from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavigationBarComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Client/food-delivery/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'home-root',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
+})
+
+export class HomeComponent{
+
+}

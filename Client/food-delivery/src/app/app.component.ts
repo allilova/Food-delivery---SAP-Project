946f8e454c9ec6ad262de832c6a2c354b3d95024
allilova <80039522+allilova@users.noreply.github.com>
2025-03-25 11:12:53 +0200

    update

--- Client/food-delivery/src/app/app.component.ts
+++ Client/food-delivery/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavigationBarComponent} from './nav-bar/nav-bar.component';
-import{FooterComponent} from './footer/footer.component';
+import {NavigationBarComponent} from './core/nav-bar/nav-bar.component';
+import{FooterComponent} from './core/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,10 @@ import {LoginComponent} from './login/login.component';
 })
 export class AppComponent {
   title = 'food-delivery';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    
+  }
 }

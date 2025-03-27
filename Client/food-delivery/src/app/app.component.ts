7d7ed2319422a2f87e716f63dcdc67baf4d20415
allilova <80039522+allilova@users.noreply.github.com>
2025-03-27 15:42:40 +0200

    add new component

--- Client/food-delivery/src/app/app.component.ts
+++ Client/food-delivery/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationBarComponent} from './core/nav-bar/nav-bar.component';
 import{FooterComponent} from './core/footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import {LoginComponent} from './login/login.component';
+import { HomeComponent } from './clients/home/home.component';
+import {LoginComponent} from './clients/login/login.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationBarComponent, FooterComponent, LoginComponent],
+  imports: [RouterOutlet, NavigationBarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

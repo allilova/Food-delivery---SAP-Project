6fe1da5f73cc1b541efb22d3115b59942c062532
allilova <80039522+allilova@users.noreply.github.com>
2025-04-11 20:28:02 +0300

    routes update

--- Client/food-delivery/src/app/app.routes.ts
+++ Client/food-delivery/src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'catalog', component: CatalogComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'search', component: SearchComponent},
     {path: '404', component: ErrorComponent},
     {path: '**', redirectTo: '/404'}
 ];

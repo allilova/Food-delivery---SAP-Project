d5fa7a2ca9b7f7587017dbaff2890891ccd28ae1
ahsenariff <ahsen.ariff@gmail.com>
2025-04-16 14:37:50 +0300

    update

--- food_delivery_app/src/main/java/com/example/food_delivery_app/model/Restaurant.java
+++ food_delivery_app/src/main/java/com/example/food_delivery_app/model/Restaurant.java
@@ -1,6 +1,5 @@
 package com.example.food_delivery_app.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;

44dc7f91b78bb0e358be217e2156df5de4bfb362
ahsenariff <ahsen.ariff@gmail.com>
2025-04-28 15:15:54 +0300

    update

--- food_delivery_app/src/main/java/com/example/food_delivery_app/repository/FoodRepository.java
+++ food_delivery_app/src/main/java/com/example/food_delivery_app/repository/FoodRepository.java
@@ -1,5 +1,6 @@
 package com.example.food_delivery_app.repository;
 
+import com.example.food_delivery_app.model.Category;
 import com.example.food_delivery_app.model.Food;
 import com.example.food_delivery_app.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,8 @@ import java.util.List;
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
     List<Food> findByMenu(Long menuId);
+
+    List<Food> findByCategory(Category category);
 
     @Query("SELECT f FROM Food f WHERE f.foodName LIKE %:keyword% OR f.category.categoryName LIKE %:keyword%")
     List<Food> searchFood(@Param("keyword")String keyword);

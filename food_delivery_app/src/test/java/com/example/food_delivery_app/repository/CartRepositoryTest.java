6f806d91bb5b761df13f45f626093ea15a338162
sezer8824 <sezerdikme2004@gmail.com>
2025-04-30 00:39:31 +0300

    PaymentRepositoryTest

--- food_delivery_app/src/test/java/com/example/food_delivery_app/repository/CartRepositoryTest.java
+++ food_delivery_app/src/test/java/com/example/food_delivery_app/repository/CartRepositoryTest.java
@@ -1,15 +1,59 @@
 package com.example.food_delivery_app.repository;
 
+import com.example.food_delivery_app.model.Cart;
+import com.example.food_delivery_app.model.Order;
+import com.example.food_delivery_app.model.Payment;
+import com.example.food_delivery_app.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
 
+
+@DataJpaTest
 class CartRepositoryTest {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @Test
-    void findByCustomer_UserID() {
+    void ItShouldFindCustomerBy_UserID() {
+        User user = new User();
+        user = userRepository.save(user);
+
+        Cart cart = new Cart();
+        cart.setCustomer(user);
+        cartRepository.save(cart);
+
+
+
+        Optional<Cart> foundCart = cartRepository.findByCustomer_UserID(user.getUserID());
+
+        assertThat(foundCart).isPresent();
+        assertThat(foundCart.get().getCustomer().getUserID()).isEqualTo(user.getUserID());
     }
 
     @Test
-    void findByCustomer_Email() {
+    void ItShouldFindCartByCustomer_Email() {
+        User user = new User();
+        user.setEmail("Sezer@gmail.com");
+        user = userRepository.save(user);
+
+        Cart cart = new Cart();
+        cart.setCustomer(user);
+        cartRepository.save(cart);
+
+        Optional<Cart> foundCart = cartRepository.findByCustomer_Email("Sezer@gmail.com");
+
+        assertThat(foundCart).isPresent();
+        assertThat(foundCart.get().getCustomer().getEmail()).isEqualTo("Sezer@gmail.com");
+
     }
 }
--- food_delivery_app/src/test/java/com/example/food_delivery_app/repository/FoodRepositoryTest.java
+++ food_delivery_app/src/test/java/com/example/food_delivery_app/repository/FoodRepositoryTest.java
@@ -1,20 +1,72 @@
 package com.example.food_delivery_app.repository;
 
+import com.example.food_delivery_app.model.Category;
+import com.example.food_delivery_app.model.Food;
+import com.example.food_delivery_app.model.Menu;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class FoodRepositoryTest {
 
+    @Autowired
+    private FoodRepository foodRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
-    void findByMenu() {
+    void itShouldFindFoodByMenu() {
+        Menu menu = new Menu();
+        entityManager.persist(menu);
+
+        Food food1 = new Food();
+        food1.setMenu(menu);
+        entityManager.persist(food1);
+
+        Food food2 = new Food();
+        food2.setMenu(menu);
+        entityManager.persist(food2);
+
+        List<Food> result = foodRepository.findByMenu(menu);
+
+        assertThat(result).hasSize(2);
+    }
+
+
+    @Test
+    void ItShouldFindByCategory() {
+        Category category= new Category();
+        entityManager.persist(category);
+
+        Food food = new Food();
+        food.setCategory(category);
+        entityManager.persist(food);
+
+        List<Food> result = foodRepository.findByCategory(category);
+
+        assertThat(result).hasSize(1);
     }
 
     @Test
-    void findByCategory() {
-    }
+    void ItShouldSearchFoodByKeyword() {
+        Category category= new Category();
+        category.setCategoryName("Italian");
+        entityManager.persist(category);
 
-    @Test
-    void searchFood() {
+        Food food = new Food();
+        food.setFoodName("Spaghetti Boloneze");
+        food.setCategory(category);
+        entityManager.persist(food);
+
+        List<Food> result = foodRepository.searchFood("Spaghetti");
+
+        assertThat(result).isNotEmpty().anyMatch(f -> f.getFoodName().contains("Spaghetti"));
     }
 }

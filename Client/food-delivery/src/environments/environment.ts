0bdd036d409aefa41a02dc5f8dc8d274288ea201
allilova <80039522+allilova@users.noreply.github.com>
2025-04-30 22:50:23 +0300

    frontend update

--- Client/food-delivery/src/environments/environment.ts
+++ Client/food-delivery/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // environment.ts - Production environment
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8083', // Use localhost for production
+    apiUrl: 'http://localhost:5454', // Use localhost for production
 };

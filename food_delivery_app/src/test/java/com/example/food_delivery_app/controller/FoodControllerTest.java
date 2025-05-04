d801be0bdb108f480da48075fec0b6246da4b24e
sezer8824 <sezerdikme2004@gmail.com>
2025-05-04 13:22:57 +0300

    new controller tests

--- food_delivery_app/src/test/java/com/example/food_delivery_app/controller/FoodControllerTest.java
+++ food_delivery_app/src/test/java/com/example/food_delivery_app/controller/FoodControllerTest.java
@@ -1,16 +1,92 @@
 package com.example.food_delivery_app.controller;
 
+import com.example.food_delivery_app.model.Menu;
+import com.example.food_delivery_app.model.USER_ROLE;
+import com.example.food_delivery_app.model.User;
+import com.example.food_delivery_app.service.FoodService;
+import com.example.food_delivery_app.service.MenuService;
+import com.example.food_delivery_app.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest(properties = {"app.jwt.secret=test-secret-key","app.jwt.expiration=3600000"})
+@AutoConfigureMockMvc
 class FoodControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private FoodService foodService;
+    @MockitoBean
+    private UserService userService;
+    @MockitoBean
+    private MenuService menuService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    private final String jwtToken = "mock-token";
+    private User mockUser() {
+        User user = new User();
+        return user;
+    }
     @Test
-    void searchFood() {
+    @WithMockUser(roles = "ADMIN")
+    void searchFoodSuccess() throws Exception {
+        when(userService.findUserByJwtToken(jwtToken)).thenReturn(mockUser());
+        when(foodService.searchFood("pizza")).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/food/search")
+                .param("foodName","pizza")
+                .header("Authorization",jwtToken))
+                .andExpect(status().isOk());
+
+    }
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void GetMenuSuccess() throws Exception {
+        Menu menu = new Menu();
+
+        when(userService.findUserByJwtToken(jwtToken)).thenReturn(mockUser());
+        when(menuService.getMenuById(anyLong())).thenReturn(menu);
+        when(foodService.getMenuFood(eq(menu),any())).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/food/menu/1")
+                .header("Authorization", jwtToken))
+                .andExpect(status().isOk());
+
     }
 
     @Test
-    void getMenuFood() {
+    @WithMockUser(roles = "ADMIN")
+    void getMenuFoodFails() throws Exception {
+        when(userService.findUserByJwtToken(jwtToken)).thenThrow(new RuntimeException("Invalid token"));
+
+        mockMvc.perform(get("/api/food/menu/1")
+                .header("Authorization", jwtToken))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void SearchFoodFails() throws Exception {
+        when(userService.findUserByJwtToken(jwtToken)).thenThrow(new RuntimeException("Invalid token"));
+
+        mockMvc.perform(get("/api/food/menu")
+                .param("foodName", "pasta")
+                .header("Authorization", jwtToken))
+                .andExpect(status().isInternalServerError());
     }
 }
--- food_delivery_app/src/test/java/com/example/food_delivery_app/controller/HomeControllerTest.java
+++ food_delivery_app/src/test/java/com/example/food_delivery_app/controller/HomeControllerTest.java
@@ -1,12 +1,33 @@
 package com.example.food_delivery_app.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest(properties = {
+        "app.jwt.secret=test-secret-key",
+        "app.jwt.expiration=3600000"
+})
+@AutoConfigureMockMvc
 class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void getHomeData() {
+    void getHomeData() throws Exception {
+        mockMvc.perform(get("/api/home"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Bunny-Fast Bites, Anytime!"))
+                .andExpect(jsonPath("$.mainText").value("Feeling hungry? We've got you covered!"))
+                .andExpect(jsonPath("$.description").value("Explore the menu, place your order, and enjoy fresh, hot, and tasty food in no time—no hassle, just great flavors."))
+                .andExpect(jsonPath("$.callToAction").value("Order now and let us handle the rest!"));
     }
 }
